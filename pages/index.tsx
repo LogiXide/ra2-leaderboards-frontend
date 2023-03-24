a989f0a38d287b93d4148227b3b120c26fdb648e
Aleksey Samokhin <lel.dude01@gmail.com>
2023-03-24 16:11:24 +0300

    feat(t36): add home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,27 @@ import type { GetStaticProps, InferGetStaticPropsType } from 'next';
 import { useTranslation, Trans } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
+import { Typography, Stack } from '@mui/material';
+
 interface ITypeProps {
   // Add custom props here
 }
 
-const Home: React.FC = (): JSX.Element => {
+const Home: React.FC = () => {
   const { t } = useTranslation('common');
 
-  return <h1>Hello World!</h1>;
+  return (
+    <Stack
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="65vh"
+    >
+      <Typography variant="h1" component="h1">
+        Hello World!
+      </Typography>
+    </Stack>
+  );
 };
 
 export const getStaticProps: GetStaticProps<ITypeProps> = async ({
